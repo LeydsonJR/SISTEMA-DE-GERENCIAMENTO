4abd75ea3e17754fea77b83ce3cfd5ea344673f7
LeydsonJR <leydsonjr02@gmail.com>
2024-06-20 21:15:54 -0300

    Cria rotas e adiciona conexao com o banco de dados

---
+++ BACK-END/src/dbConnection.js
@@ -0,0 +1,20 @@
+const pg = require('pg');
+const { Pool, Client } = pg;
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'root',
+  host: '',
+  port: 5432,
+  database: 'Back-GEN',
+});
+
+const client = new Client({
+  user: 'postgres',
+  password: 'root',
+  host: '',
+  port: 5432,
+  database: 'Back-GEN',
+});
+
+module.exports = client;
--- BACK-END/src/index.js
+++ BACK-END/src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
+const client = require('./dbConnection');
 app.use(express.json());
 
 app.post('/api/login', async (req, res) => {
   return res.send('Hello world');
 });
 
-app.post('/api/users', async (req, res) => {
-  return res.send('Hello world');
+app.post('/api/schools', async (req, res) => {
+  const { nome, endereco } = req.body;
+  await client.connect();
+  await client.query('INSERT INTO escola (nome, endereco) VALUES ($1, $2)', [
+    nome,
+    endereco,
+  ]);
+  res.status(201).send();
 });
 
-app.post('/api/schools', async (req, res) => {
+app.post('/api/users', async (req, res) => {
   return res.send('Hello world');
 });
 
